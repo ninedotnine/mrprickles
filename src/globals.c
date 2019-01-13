f179c58fe286acf08f53089be257efc5190d3092
dan soucy <dev@danso.ca>
2019-01-12 19:28:20 -0500

    reorganize global settings

--- src/globals.c
+++ src/globals.c
@@ -5,6 +5,9 @@ const char * const mrprickles_statuses[] = {
     "a humorously-named cactus from australia",
     "i am a robot pretending to be a cactus"
 };
+const char * const help_msg = "list of commands:\ninfo: show stats.\ncallme: launch an audio call.\n"
+    "videocallme: launch a video call.\nonline/away/busy: change my user status\nname: change my name\n"
+    "status: change my status message";
 
 time_t last_info_change; // always a timestring
 time_t start_time;
@@ -16,6 +19,3 @@ const int32_t video_bitrate = 5000;
 ToxAV *g_toxAV = NULL;
 pthread_t main_thread;
 
-const char *help_msg = "list of commands:\ninfo: show stats.\ncallme: launch an audio call.\n"
-    "videocallme: launch a video call.\nonline/away/busy: change my user status\nname: change my name\n"
-    "status: change my status message";
--- src/globals.h
+++ src/globals.h
@@ -10,11 +10,12 @@
 
 #define MRPRICKLES_VERSION "mrprickles version 0.3.1"
 
-// reset name and status message every 6 hours
-#define RESET_INFO_DELAY 21600
-
 extern const char * const mrprickles_name;
 extern const char * const mrprickles_statuses[2];
+extern const char * const help_msg;
+
+// reset name and status message every 6 hours
+#define RESET_INFO_DELAY 21600
 
 extern time_t last_info_change; // always a timestring
 extern time_t start_time;
@@ -25,5 +26,3 @@ extern const int32_t video_bitrate;
 
 extern ToxAV *g_toxAV;
 extern pthread_t main_thread;
-
-extern const char *help_msg;
